4b41f7d6ae0cc021501ff1358e93d550caf8f2d2
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-03-31 08:57:52 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,273 @@
+const elList = document.querySelector('#post-list');
+let idxs = 0;
+object.forEach((item, index) => {
+  let li = document.createElement("li");
+  let div = document.createElement('div');
+  li.className = `post__item col-4 onclick="func('${item.id}')"`;
+  li.setAttribute("data-bs-toggle","modal");
+  li.setAttribute("data-bs-target","#staticBackdrop");
+  if(item.imgs[0].includes('.mp4')){
+    li.innerHTML = `
+          <div class="post__box">
+             <img src="${item.imgs[0]}" alt="img">
+            <div class="post__hover"></div>
+            <div class="post__icons d-flex">
+              <div class="post__like d-flex align-items-center">
+                <i class='bx bxs-heart post__icon'></i>
+                <span class="post__text">${item.likes}</span>
+              </div>
+              <div class="post__comment d-flex align-items-center">
+                <i class='bx bxs-message-rounded post__icon'></i>
+                <span class="post__text">${item.comment}</span>
+              </div>
+            </div>
+          </div>
+    
+  `;
+    div.innerHTML = `
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="d-flex col-12">
+              <div class="modal__imgbox col-6">
+                <video class="video__box" controls>
+                  <source src="${item.imgs[0]}" type="video/mp4">
+                </video>
+              </div>
+              <div class="modal-info d-flex flex-column justify-content-between col-6">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="modal__ikons">
+                    <button class="modal__btn">
+                      <i id="like" class='bx bx-heart modal__ikon modal__heart' ></i>
+                    </button>
+                    <button class="modal__btn">
+                      <i class='bx bx-message-rounded modal__ikon' ></i>
+                    </button>
+                    <button class="modal__btn">
+                      <i class='bx bx-navigation modal__ikon' ></i>
+                    </button>
+                  </div>
+                  <div class="modal__bookmark">
+                    <button class="modal__book onclick="func('${item.id}')"">
+                      <i id="save" class='bx bx-bookmark modal__ikon modal__save'></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-header">
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+      </div>
+    </div>
+    
+    `;
+  } else {
+    li.innerHTML = `
+    <div class="post__box">
+            <div class="post__imgbox">
+              <img src="${item.imgs[0]}" alt="img">
+            </div>
+            <div class="post__hover"></div>
+            <div class="post__icons d-flex">
+              <div class="post__like d-flex align-items-center">
+                <i class='bx bxs-heart post__icon'></i>
+                <span class="post__text">${item.likes}</span>
+              </div>
+              <div class="post__comment d-flex align-items-center">
+                <i class='bx bxs-message-rounded post__icon'></i>
+                <span class="post__text">${item.comment}</span>
+              </div>
+            </div>
+          </div>
+          
+          
+  `; 
+    div.innerHTML = `
+      <!-- Modal -->
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-body">
+              <div class="d-flex col-12">
+                <div class="modal-imgbox col-6">
+                  <img class="modal-img" src="${item.imgs[0]}" alt="img">
+                </div>
+                <div class="modal-info d-flex flex-column justify-content-between col-6">
+                  <p>Lorem ipsum dolor sit amet.</p>
+                  <div class="d-flex align-items-center justify-content-between">
+                    <div class="modal__ikons">
+                      <button class="modal__btn">
+                        <i id="like" class='bx bx-heart modal__ikon modal__heart' ></i>
+                      </button>
+                      <button class="modal__btn">
+                        <i class='bx bx-message-rounded modal__ikon' ></i>
+                      </button>
+                      <button class="modal__btn">
+                        <i class='bx bx-navigation modal__ikon' ></i>
+                      </button>
+                    </div>
+                    <div class="modal__bookmark">
+                      <button class="modal__book">
+                        <i id="save" class='bx bx-bookmark modal__ikon modal__save'></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-header">
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+        </div>
+      </div>
+      
+    `;
+  }
+  elList.appendChild(div);
+  elList.appendChild(li);
+});
+
+const canvasList = document.getElementById("canvas-list");
+const elSave = document.getElementById("save");
+
+
+object.forEach((item, index) => {
+  let li = document.createElement("li");
+  let div = document.createElement('div');
+  li.className = `post__item col-4`;
+  li.setAttribute("data-bs-toggle","modal");
+  li.setAttribute("data-bs-target","#staticBackdrop");
+  if(item.imgs[0].includes('.mp4')){
+    li.innerHTML = `
+          <div class="post__box">
+             <img src="${item.imgs[1]}" alt="img">
+            <div class="post__hover"></div>
+            <div class="post__icons d-flex">
+              <div class="post__like d-flex align-items-center">
+                <i class='bx bxs-heart post__icon'></i>
+                <span class="post__text">${item.likes}</span>
+              </div>
+              <div class="post__comment d-flex align-items-center">
+                <i class='bx bxs-message-rounded post__icon'></i>
+                <span class="post__text">${item.comment}</span>
+              </div>
+            </div>
+          </div>
+    
+  `;
+    div.innerHTML = `
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="d-flex col-12">
+              <div class="modal__imgbox col-6">
+                <video class="video__box" controls>
+                  <source src="${item.imgs[0]}" type="video/mp4">
+                </video>
+              </div>
+              <div class="modal-info d-flex flex-column justify-content-between col-6">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="modal__ikons">
+                    <button class="modal__btn">
+                      <i id="like" class='bx bx-heart modal__ikon modal__heart' ></i>
+                    </button>
+                    <button class="modal__btn">
+                      <i class='bx bx-message-rounded modal__ikon' ></i>
+                    </button>
+                    <button class="modal__btn">
+                      <i class='bx bx-navigation modal__ikon' ></i>
+                    </button>
+                  </div>
+                  <div class="modal__bookmark">
+                    <button class="modal__book">
+                      <i id="save" class='bx bx-bookmark modal__ikon modal__save'></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-header">
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+      </div>
+    </div>
+    
+    `;
+  } else {
+    li.innerHTML = `
+    <div class="post__box">
+            <div class="post__imgbox">
+              <img src="${item.imgs[0]}" alt="img">
+            </div>
+            <div class="post__hover"></div>
+            <div class="post__icons d-flex">
+              <div class="post__like d-flex align-items-center">
+                <i class='bx bxs-heart post__icon'></i>
+                <span class="post__text">${item.likes}</span>
+              </div>
+              <div class="post__comment d-flex align-items-center">
+                <i class='bx bxs-message-rounded post__icon'></i>
+                <span class="post__text">${item.comment}</span>
+              </div>
+            </div>
+          </div>
+          
+          
+  `; 
+  div.innerHTML = `
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="d-flex col-12">
+              <div class="modal-imgbox col-6">
+                <img class="modal-img" src="${item.imgs[0]}" alt="img">
+              </div>
+              <div class="modal-info d-flex flex-column justify-content-between col-6">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="modal__ikons">
+                    <button class="modal__btn">
+                      <i id="like" class='bx bx-heart modal__ikon modal__heart' ></i>
+                    </button>
+                    <button class="modal__btn">
+                      <i class='bx bx-message-rounded modal__ikon' ></i>
+                    </button>
+                    <button class="modal__btn">
+                      <i class='bx bx-navigation modal__ikon' ></i>
+                    </button>
+                  </div>
+                  <div class="modal__bookmark">
+                    <button class="modal__book">
+                      <i id="save" class='bx bx-bookmark modal__ikon modal__save'></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-header">
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+      </div>
+    </div>
+    
+    `;
+  }
+  canvasList.appendChild(div);
+  canvasList.appendChild(li);
+});
---
+++ js/main.js
@@ -0,0 +1,82 @@
+const object = [
+  {
+    id: 1,
+    imgs: [
+      // '../videos/data.mp4',
+      'https://picsum.photos/id/30/300/350',
+    ],
+    title: "Muhammadali Eshonqulov bilan suhbat",
+    desc: "Najot Talim jamoasi oz oquvchilari va kuzatuvchilari uchun foydali bolgan suhbatlarni uyushtirishda davom etadi. Bu galgi mehmonimiz «Osmondagi bolalar» loyihasining 10-son mehmoni, «Yuksalish» liderlar maktabi asoschisi, xalqaro toifadagi iqtisodchi Muhammadali Eshonqulov boladi",
+    likes: 500,
+    comment: 60,
+    save: false,
+  },
+  {
+    id: 2,
+    imgs: [
+      'https://picsum.photos/id/341/300/350',
+      // '../videos/data.mp4',
+    ],
+    title: "Dizayn foundation bosqichi kirish imtihonining natijalarini e'lon qilamiz",
+    desc: "Najot Talim jamoasi oz oquvchilari va kuzatuvchilari uchun foydali bolgan suhbatlarni uyushtirishda davom etadi. Bu galgi mehmonimiz «Osmondagi bolalar» loyihasining 10-son mehmoni, «Yuksalish» liderlar maktabi asoschisi, xalqaro toifadagi iqtisodchi Muhammadali Eshonqulov boladi",
+    likes: 200,
+    comment: 18,
+    save: false,
+  },
+
+  {
+    id: 3,
+    imgs: [
+      'https://picsum.photos/id/89/300/350',
+      // 'https://picsum.photos/id/100/300/350',
+    ],
+    title: "Dasturchilar uchun o'z sohalariga oid foydali kitoblar to'plami",
+    desc: "Yodingizda bo'lsa, avvalroq grafik dizaynerlar o'qishi kerak bo'lgan kitoblar to'plamini tavsiya qilgan edik. Ushbu postning izohlar qismida kuzatuvchilarimiz tomonidan dasturchilar uchun ham kitob tavsiya qilishimiz so'ralgan edi Obunachilarimizning fikrlarini inobatga olgan holda, bu gal dasturchilar o'qishi kerak bo'lgan kitoblar to'plamini tavsiya qilishga qaror qildik. Ushbu kasb egalari uchun o'z sohalariga doir ushbu kitoblar foydali bo'ladi, degan umiddamiz. 😊",
+    likes: 200,
+    comment: 90,
+    save: false,
+  },
+
+  {
+    id: 4,
+    imgs: [
+      'https://picsum.photos/id/10/300/350',
+      // '../videos/data-two.mp4',
+      // 'https://picsum.photos/id/90/300/350',
+    ],
+    title: "Grafik dizaynerlarga ham oson emas!",
+    desc: "Har bir kasbda bo'lgani kabi grafik dizayn sohasining ham o'ziga yarasha qiyinchiliklari va sabr qilishi kerak bo'lgan nuqtalari mavjud. Bulardan eng asosiysi mijozlar bilan ishlash boʻlsa kerak. O'ylaymizki, bu gapimizni barcha grafik dizaynerlar ham tasdiqlashadi. 😉🎯 Shunday qilib, bugun dizayner va mijoz o'rtasidagi ayrim tushunmovchiliklar haqida suhbat qilamiz.",
+    likes: 30,
+    comment: 28,
+    save: false,
+  },
+
+  {
+    id: 5,
+    imgs: [
+      'https://picsum.photos/id/950/300/350',
+      // 'https://picsum.photos/id/140/300/350',
+      // 'https://picsum.photos/id/160/300/350',
+      // 'https://picsum.photos/id/170/300/350',
+      // 'https://picsum.photos/id/180/300/350',
+    ],
+    title: "Ta'til kunlarini maroqli o'tkazing",
+    desc: "Xabaringiz bor, markazimiz o'quvchilari uchun 20-21-22-mart sanalari bahorgi ta'til kunlari deb e'lon qilingan edi.Kuzatuvchilarimiz va o'quvchilarimizning ta'til kunlarini mazmunli o'tkazishlari uchun markazimizda bo'lib o'tgan so'nggi master-klass videolarini tavsiya qilishga qaror qildik. O'qish va ish bilan bo'lib, premyeralarni o'tkazib yuborgan bo'lsangiz, ushbu ta'til kunlari ajoyib imkoniyat. 😉",
+    likes: 50,
+    comment: 12,
+    save: false,
+  },
+
+  {
+    id: 6,
+    imgs: [
+      'https://picsum.photos/id/70/300/350'
+    ],
+    title: "Muhammadali Eshonqulov bilan suhbat",
+    desc: "Najot Talim jamoasi oz oquvchilari va kuzatuvchilari uchun foydali bolgan suhbatlarni uyushtirishda davom etadi. Bu galgi mehmonimiz «Osmondagi bolalar» loyihasining 10-son mehmoni, «Yuksalish» liderlar maktabi asoschisi, xalqaro toifadagi iqtisodchi Muhammadali Eshonqulov boladi",
+    likes: 32,
+    comment: 29,
+    save: false,
+
+  }
+];
